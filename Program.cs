1f4396d78e6f1666b264e9c88a0c1396ce3231a4
Stuart Shepherd <stuartshepherd@gmail.com>
2020-03-05 20:25:12 +1100

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace RunTimePolymorphism
+{
+    class Program
+    {
+        // Base Class
+        public class BaseClass
+        {
+            public virtual void GetInfo()
+            {
+                Console.WriteLine("Base Class Info");
+            }
+        }
+
+        // Derived Class
+        public class DerivedClass : BaseClass
+        {
+            public override void GetInfo()
+            {
+                Console.WriteLine("Derived Class Info");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Run Time Polymorphism");
+            Console.WriteLine("Polymorphism provides an ability for the classes to implement different methods that are called through the same name ");
+            Console.WriteLine("and it also provides an ability to invoke the methods of a derived class through base class reference during runtime based on our requirements.");
+            Console.WriteLine("Run Time Polymorphism means overriding a base class method in the derived class by creating a similar function and this can ");
+            Console.WriteLine("be achieved by using override & virtual keywords along with inheritance principle.");
+            Console.WriteLine("");
+            
+            DerivedClass d = new DerivedClass();
+            d.GetInfo();
+
+            BaseClass b = new BaseClass();
+            b.GetInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Run Time Polymorphism Example
+Polymorphism provides an ability for the classes to implement different methods that are called through the same name and it also provides an ability to invoke the methods of a derived class through base class reference during runtime based on our requirements.
+
+Run Time Polymorphism means overriding a base class method in the derived class by creating a similar function and this can be achieved by using override & virtual keywords along with inheritance principle.
+
+## Getting Started
+
+### Prerequisites
+
+[.NET Core 3.1 SDK or later](https://dotnet.microsoft.com/download/dotnet-core/3.1)
